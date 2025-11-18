638917a4314b9828fd1ce27c6cbe5e6e847bc3a4
junHyeong7083 <parkjunhyeong0902@gmail.com>
2025-11-18 21:54:21 +0900

    Feat API Config
    SO를 통해 API키 인증

---
+++ Assets/01_Script/Config/API.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "API", menuName = "Config/API")]
+public class API : ScriptableObject
+{
+    public string Runway_API;
+    public string ChatGPT_API;
+    public string Whisk_API;
+}
